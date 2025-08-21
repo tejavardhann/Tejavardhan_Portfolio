75c12528c6ea1760cb7ad9b112a8556cf23e3a38
tejavardhann <tejvardhan119@gmail.com>
2025-08-21 18:44:01 -0500

    Update vite.config.ts

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/tejavardhan-portfolio/",   // 👈 IMPORTANT: repo name here
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-});
+})
